246217f33364c5b7eb84f3f1d1b1ce14671241c9
Jan Esser <JEsser@gmx.de>
2024-12-08 22:20:40 +0100

    add git-confined contents

--- try-build.sh
+++ try-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
-ECLIPSE_PACKAGE=$1
+if [ -n $1 ]; then
+    ECLIPSE_PACKAGE=$1
+fi
+
 export ECLIPSE_PACKAGE=${ECLIPSE_PACKAGE:='eclipse-java'}
 
-./set-eclipse-package.sh
+. ./set-eclipse-package.sh
 
 # snapcraft clean
 # sudo less /var/snap/lxd/common/lxd/logs/lxd.log
@@ -12,9 +15,11 @@ snapcraft try --debug
 
 sudo snap try prime # --devmode
 
-sudo snap connect $ECLIPSE_PACKAGE:personal-git-global
+sudo snap connect $ECLIPSE_PACKAGE:personal-gitconfig
+sudo snap connect $ECLIPSE_PACKAGE:personal-sshid
 sudo snap connect $ECLIPSE_PACKAGE:personal-maven-cache
 sudo snap connect $ECLIPSE_PACKAGE:personal-eclipse-config
 sudo snap connect $ECLIPSE_PACKAGE:sources-dir
+sudo snap connect $ECLIPSE_PACKAGE:git-plug git-confined
 
-snap run $ECLIPSE_PACKAGE
+# snap run $ECLIPSE_PACKAGE
